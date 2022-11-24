c4c7abc036c628c50c047d79978713d71386dcfb
Jody VanHoose <jodyvanhoose80@gmail.com>
2022-11-24 15:17:37 -0500

    fixed article links, code cleanup, added readme

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const xMenu = document.getElementById('menu-close')
 
 // mobile open menu function
 const openMenu = () =>{
-  // hamMenu.style.display = 'none'
   navMenu.className = 'isOpen'
   body.style.backgroundColor = '#c5c5c5'
   body.classList.add("stop-scrolling")
@@ -14,7 +13,6 @@ const openMenu = () =>{
 
 // mobile close menu function
 const closeMenu = () =>{
-  // hamMenu.style.display = 'block'
   navMenu.remove.className = 'isOpen'
   navMenu.className = 'isClosed'
   body.style.backgroundColor = '#fff'
